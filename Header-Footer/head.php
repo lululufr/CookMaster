e4267c65a634d3db94431f86bd94b49635478c93
lululufr <91730933+lululufr@users.noreply.github.com>
2023-04-14 12:57:53 +0200

    Add - Header - Footer
    Ajout des éléments de style principaux

---
+++ Header-Footer/head.php
@@ -0,0 +1,12 @@
+<html lang="fr" data-theme="dracula">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!--daisy UI-->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.51.5/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <title>Cook Master</title>
+</head>
--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cook Master</title>
-</head>
-<body>
-    
+
+<?php include("./Header-Footer/head.php");?>
+
+
+<body >
+
+<?php include("./Header-Footer/header.php");?>
+
+
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hello there</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+
+
+<?php include("./Header-Footer/footer.php");?>
 </body>
 </html>
