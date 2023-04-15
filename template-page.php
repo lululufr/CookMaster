5806a3010471fed406aa26006a8c6c5369d699a0
lululufr <91730933+lululufr@users.noreply.github.com>
2023-04-15 17:47:56 +0200

    add - curl programm in C

--- template-page.php
+++ template-page.php
@@ -15,3 +15,21 @@ require "./Function/function.php";
 <?php include("./Header-Footer/footer.php");?>
 </body>
 </html>
+
+
+
+<?php 
+//template BDD query
+$pdo = connectDB();
+
+					$query = $pdo->prepare("SELECT * FROM USER WHERE id=:id");
+          
+					$query->execute(["id"=>1]);
+					$user = $query->fetch();
+
+          foreach($user as $elem){
+            echo("<p>".$elem."</p>");
+          }
+          
+
+?>
