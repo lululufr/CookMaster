b7505fbb1ec0184a853e2cd87f4dbcff41a8d3b4
lululufr <lucas.lm.miller@gmail.com>
2023-04-15 18:10:36 +0200

    Changes by Alexis

--- API/REQUESTER/main.c
+++ API/REQUESTER/main.c
@@ -3,28 +3,29 @@
 
 int main(int argc, char* argv[])
 {
-  CURL *curl;
-  CURLcode res;
+    CURL *curl;
+    CURLcode res;
 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl_global_init(CURL_GLOBAL_DEFAULT);
 
-  curl = curl_easy_init();
+    curl = curl_easy_init();
 
+    printf("test : %s",argv[1]);
 
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, argv[0]);
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    if(curl)  {
+        curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-    res = curl_easy_perform(curl);
-    if(res != CURLE_OK) {
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK) {
+          fprintf(stderr, " erreur _>  %s\n",
+                  curl_easy_strerror(res));
+        }
+
+        curl_easy_cleanup(curl);
     }
 
-    curl_easy_cleanup(curl);
-  }
+    curl_global_cleanup();
 
-  curl_global_cleanup();
-
-  return 0;
+    return 0;
 }
