2d105039e242a7cc2e750d501a822d82646a7f0d
lululufr <lucas.lm.miller@gmail.com>
2023-05-08 18:11:42 +0200

    Changes by Alexis

--- resources/views/users/profil.blade.php
+++ resources/views/users/profil.blade.php
@@ -1,17 +1,78 @@
+@if(session('message'))
+    <div class="alert alert-success">
+        Profil modifiés avec succes
+    </div>
+@endif
 <x-header/>
+<div class="form-control grid justify-items-center m-4 w-full max-w-xs">
+    <form method="get">
+        @csrf
+        <div class="form-control w-full max-w-xs m-5">
+            <img src="{{$infos['profil_picture']}}" alt="error" style="max-width: 40%;"/>
+            <input type="image" name="img" id="profil_picture" >
+            @error('pseudonyme')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
+        </div>
 
-        {{$infos['id']}}
-        {{$infos['username']}}
-        {{$infos['email']}}
+        <div class="form-control w-full max-w-xs m-5">
+            <label>Pseudonyme</label>
+            <input type="text" name="username" placeholder="{{$infos['username']}}"
+                   class="input w-full max-w-xs
+                          @error('pseudonyme') is-invalide @enderror"/>
 
-        {{$infos['created_at']}}
-        {{$infos['updated_at']}}
+            @error('pseudonyme')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="form-control w-full max-w-xs m-5">
+        <label>Prénom</label>
+            <input type="text" name="firstname" placeholder="{{$infos['firstname']}}"
+                   class="input w-full max-w-xs
+                         @error('firstname') is-invalide @enderror"/>
 
-        {{$infos['role']}}
+            @error('firstname')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-        {{$infos['buying_plan']}}
+        <div class="form-control w-full max-w-xs m-5">
+        <label >Prénom</label>
+            <input type="text" name="lastname" placeholder="{{$infos['lastname']}}"
+                   class="input w-full max-w-xs
+                         @error('lastname') is-invalide @enderror"/>
 
+            @error('lastname')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
+        <div class="form-control w-full max-w-xs m-5">
+        <label >E-mail</label>
+        <input type="text" name="email" placeholder="{{$infos['email']}}"
+               class="input w-full max-w-xs
+                     @error('email') is-invalide @enderror"/>
 
+        @error('email')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        </div>
+
+        <div class="form-control w-full max-w-xs m-5">
+            <label >Mot de passe</label>
+            <input type="password" name="email" placeholder="XXXX"
+                   class="input w-full max-w-xs
+                         @error('password') is-invalide @enderror"/>
+
+            @error('password')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <input type="submit">
+    </form>
+</div>
+<div></div>
+dernière modification{{$infos['updated_at']}}
+compte créé le : {{$infos['created_at']}}
 <x-footer/>
