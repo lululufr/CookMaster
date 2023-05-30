22b7a8a5ff43e37e0f0277192cfda2a0e2ff47d2
alexis elef <alexis.elefteriou.pro@gmail.com>
2023-05-31 00:44:19 +0200

    functionnal

--- API/REQUESTER/requester-API.c
+++ API/REQUESTER/requester-API.c
@@ -1,50 +1,67 @@
+#include <gtk/gtk.h>
 #include <stdio.h>
 #include <curl/curl.h>
-#include <gtk/gtk.h>
-//gcc `pkg-config --cflags gtk+-3.0` -o VisualRequester requester-API.c `pkg-config --libs gtk+-3.0 libcurl` <<< pour compiler
+#include <stdlib.h>
+#include <string.h>
+
+const gchar *apiValue = NULL;
+const gchar *keyvalue = NULL;
 
 typedef struct {
-    GtkWidget *KeyEntry;
-    GtkWidget *URIEntry;
-} EntryData;
+    GtkWidget * textOutput;
+    GtkTextBuffer * buffer;
+}Data;
 
-void on_RequestButton_Click(GtkWidget *widget, gpointer data) {
-    EntryData *entryData = (EntryData *)data;
+size_t write_callback(void *ptr, size_t size, size_t nmemb, char *output) {
+    size_t total_size = size * nmemb;
+    // Append the received data to the output buffer
+    strncat(output, ptr, total_size);
+    return total_size;
+}
 
-    GtkWidget *KeyEntry = entryData->KeyEntry;
-    GtkWidget *URIEntry = entryData->URIEntry;
+static void print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+    Data *viewData = (Data *)data;
 
-    const gchar *key = gtk_entry_get_text(GTK_ENTRY(KeyEntry));
-    const gchar *URI = gtk_entry_get_text(GTK_ENTRY(URIEntry));
-    GtkTextBuffer *OutputBuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(data));
+    GtkTextBuffer *buffer = viewData->buffer;
+    GtkWidget *textOutput = viewData->textOutput;
 
-    // CURL
     CURL *curl;
     CURLcode res;
-    gchar output[4096];
-
+    char *key = "8299dbe30a5a5362b7e24b8adf23b20d";
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
+    char *output = malloc(sizeof(char) * 8192);
+    output[0] = '\0';  // Initialize the output buffer
+
+
 
     if (curl) {
         struct curl_slist *headers = NULL;
-        headers = curl_slist_append(headers, "X-API-Key: your_api_key"); // Exemple d'en-tête
-        headers = curl_slist_append(headers, "Authorization: Bearer your_token"); // Exemple d'en-tête
+        char * header = malloc(sizeof (char )*267);
+        sprintf(header, "X-API-Key: %s", keyvalue);
 
-        curl_easy_setopt(curl, CURLOPT_URL, URI);
+        headers = curl_slist_append(headers, header);
+        sprintf(header, "Authorization: Bearer %s", keyvalue);
+        headers = curl_slist_append(headers, header);
+        free(header);
+
+        curl_easy_setopt(curl, CURLOPT_URL, apiValue);
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-        // Capturer la sortie dans la variable 'output'
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+        // Set the callback function and pass the output buffer as userdata
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, output);
 
         res = curl_easy_perform(curl);
         if (res != CURLE_OK) {
             fprintf(stderr, "Error: %s\n", curl_easy_strerror(res));
         } else {
-            // Définir le texte du GtkTextBuffer associé à OutputText
-            gtk_text_buffer_set_text(OutputBuffer, output, -1);
+            gtk_text_buffer_set_text(buffer, output, -1);
+
+            gtk_widget_queue_draw(textOutput);
         }
 
         curl_easy_cleanup(curl);
@@ -52,44 +69,63 @@ void on_RequestButton_Click(GtkWidget *widget, gpointer data) {
     }
 
     curl_global_cleanup();
+
+    free(output); // Free the allocated memory for output
 }
 
-int main(int argc, char* argv[]) {
-    // GTK initialisation des éléments de la fenetre
-    GtkWidget *fenetre_principale = NULL;
-    GtkWidget *URIEntry = NULL;
-    GtkWidget *KeyEntry = NULL;
-    GtkTextView *OutputText = NULL;
-    GtkWidget *RequestButton = NULL;
-    GtkBuilder *builder = NULL;
+void on_api_changed(GtkEntry *entry, gpointer data) {
+    apiValue = gtk_entry_get_text(entry);
+}
+void on_key_changed(GtkEntry *entry, gpointer data) {
+    keyvalue = gtk_entry_get_text(entry);
+}
+
+int main (int   argc,
+      char *argv[])
+{
+    GtkBuilder *builder;
+    GObject *window;
+    GObject *button;
+    GtkWidget *APIEntry;
+    GtkWidget *KeyEntry;
+    GtkWidget *textOutput;
+    GtkTextBuffer *buffer;
     GError *error = NULL;
-    gchar *filename = NULL;
 
-    gtk_init(&argc, &argv);
-    // chargement du fichier .glade
-    builder = gtk_builder_new();
-    filename = g_build_filename("./visualApp.glade", NULL);
+    gtk_init (&argc, &argv);
 
-    gtk_builder_add_from_file(builder, filename, &error);
-    g_free(filename);
-    //affectation des éléments du fichier
-    fenetre_principale = GTK_WIDGET(gtk_builder_get_object(builder, "Window"));
-    URIEntry = GTK_WIDGET(gtk_builder_get_object(builder, "URIEntry"));
+    /* Construct a GtkBuilder instance and load our UI description */
+    builder = gtk_builder_new ();
+    if (gtk_builder_add_from_file (builder, "builder.ui", &error) == 0)
+    {
+        g_printerr ("Error loading file: %s\n", error->message);
+        g_clear_error (&error);
+        return 1;
+    }
+
+    /* Connect signal handlers to the constructed widgets. */
+    window = gtk_builder_get_object (builder, "window");
+    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+
+    APIEntry = GTK_WIDGET(gtk_builder_get_object(builder, "APIEntry"));
+    g_signal_connect(APIEntry, "changed", G_CALLBACK(on_api_changed), (gpointer ) NULL);
+
     KeyEntry = GTK_WIDGET(gtk_builder_get_object(builder, "KeyEntry"));
-    RequestButton = GTK_WIDGET(gtk_builder_get_object(builder, "RequestButton"));
-    OutputText = GTK_TEXT_VIEW(gtk_builder_get_object(builder, "OutputText"));
+    g_signal_connect(KeyEntry, "changed", G_CALLBACK(on_key_changed), (gpointer ) NULL);
+
+    textOutput = GTK_WIDGET(gtk_builder_get_object(builder, "textOutput"));
+    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textOutput));
+    Data data;
+    data.textOutput = textOutput;
+    data.buffer = buffer;
+
+    button = gtk_builder_get_object (builder, "button");
+
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), &data);
 
 
-    //entrée dans la structure des pointeurs
-    EntryData entryData;
-    entryData.KeyEntry = KeyEntry;
-    entryData.URIEntry = URIEntry;
+    gtk_main ();
 
-    //création des signaux
-    gtk_builder_connect_signals(builder, NULL);
-    g_signal_connect(fenetre_principale, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(RequestButton, "clicked", G_CALLBACK(on_RequestButton_Click), (gpointer)&entryData);
-
-    gtk_widget_show_all(fenetre_principale);
-    gtk_main();
+    return 0;
 }
