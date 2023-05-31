0ff1035ac148e00a11dc6ab73142193186e973ea
Karl <oelschlager.karl@gmail.com>
2023-05-31 20:48:19 +0200

    Page d'accueil nouveaux articles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,48 +6,48 @@
         <article class="flex flex-col shadow my-4">
             <!-- Article Image -->
             <a href="#" class="hover:opacity-75">
-                <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1">
+                <img src="https://media.istockphoto.com/id/1154731746/fr/photo/cheeseburger-au-cola-et-frites-fran%C3%A7ais.jpg?s=612x612&w=0&k=20&c=yEvgjV_otTTBEqvxFNl2y_bV6D_US9P0OF-fFtoNQls=">
             </a>
             <div class="bg-white flex flex-col justify-start p-6">
-                <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-                <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
+                <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Santé</a>
+                <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">Les maux causés par les fast-foods : un défi pour la santé publique</a>
                 <p href="#" class="text-sm pb-3">
-                    By <a href="#" class="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
+                    By <a href="#" class="font-semibold hover:text-gray-800">David Grzyb</a>, Publié le 3 Mars 2023
                 </p>
-                <a href="#" class="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-                <a href="#" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
+                <a href="#" class="pb-6">Les fast-foods sont devenus omniprésents dans nos vies modernes. Leur accessibilité, leur prix abordable et leur commodité en ont fait une option de repas populaire pour de nombreuses personnes. Cependant, derrière la facilité et la rapidité offertes par ces établissements, se cachent de nombreux maux pour la santé. Dans cet article, nous allons explorer les conséquences néfastes des fast-foods sur la santé publique.</a>
+                <a href="#" class="uppercase text-gray-800 hover:text-black">Continuer de lire...<i class="fas fa-arrow-right"></i></a>
             </div>
         </article>
 
         <article class="flex flex-col shadow my-4">
             <!-- Article Image -->
             <a href="#" class="hover:opacity-75">
-                <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=2">
+                <img src="https://agriculture.gouv.fr/sites/default/files/styles/node_visuel/public/19173_026_bd.jpg?itok=Q7PUWO1X">
             </a>
             <div class="bg-white flex flex-col justify-start p-6">
-                <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Automotive, Finance</a>
-                <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
+                <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Environnement</a>
+                <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">Les bienfaits environnementaux de l'agriculture biologique : une alternative durable à l'agriculture classique</a>
                 <p href="#" class="text-sm pb-3">
-                    By <a href="#" class="font-semibold hover:text-gray-800">David Grzyb</a>, Published on January 12th, 2020
+                    By <a href="#" class="font-semibold hover:text-gray-800">Elizabeth Huxley</a>, Publié le 12 Janvier 2023
                 </p>
-                <a href="#" class="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-                <a href="#" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
+                <a href="#" class="pb-6">L'agriculture biologique est de plus en plus reconnue comme une alternative durable à l'agriculture conventionnelle. En utilisant des pratiques agricoles respectueuses de l'environnement, l'agriculture biologique offre de nombreux avantages en termes de préservation des écosystèmes, de protection de la biodiversité et de réduction de l'impact sur le climat. Dans cet article, nous allons explorer les bienfaits environnementaux de l'agriculture biologique par rapport à l'agriculture classique.</a>
+                <a href="#" class="uppercase text-gray-800 hover:text-black">Continuer de lire...<i class="fas fa-arrow-right"></i></a>
             </div>
         </article>
 
         <article class="flex flex-col shadow my-4">
             <!-- Article Image -->
             <a href="#" class="hover:opacity-75">
-                <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=3">
+                <img src="https://www.celnat.fr/wp-content/uploads/2020/09/Vegetarisme-head.jpg">
             </a>
             <div class="bg-white flex flex-col justify-start p-6">
-                <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Sports</a>
-                <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
+                <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Cuisine, Santé</a>
+                <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">Les végétariens et les alternatives à la viande : une révolution culinaire</a>
                 <p href="#" class="text-sm pb-3">
-                    By <a href="#" class="font-semibold hover:text-gray-800">David Grzyb</a>, Published on October 22nd, 2019
+                    By <a href="#" class="font-semibold hover:text-gray-800">Jerôme Gratt</a>, Publié le 22 Octobre 2022
                 </p>
-                <a href="#" class="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-                <a href="#" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
+                <a href="#" class="pb-6">Le végétarisme gagne en popularité dans le monde entier, alimenté par des préoccupations pour la santé, l'éthique animale et l'impact environnemental de l'industrie de la viande. Cette tendance a conduit à l'émergence de nombreuses alternatives à la viande, offrant aux végétariens et aux personnes souhaitant réduire leur consommation de viande une multitude d'options culinaires. Dans cet article, nous explorerons le monde des végétariens et les alternatives à la viande qui transforment la cuisine moderne.</a>
+                <a href="#" class="uppercase text-gray-800 hover:text-black">Continuer de lire...<i class="fas fa-arrow-right"></i></a>
             </div>
         </article>
 
@@ -64,28 +64,28 @@
     <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
 
         <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-            <p class="text-xl font-semibold pb-5">About Us</p>
-            <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
+            <p class="text-xl font-semibold pb-5">A propos de nous</p>
+            <p class="pb-2">Cookmaster est une entreprise de partage de connaissances et d'expériences culinaires ! En cuisine !</p>
             <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
-                Get to know us
+                Apprenez à nous connaître
             </a>
         </div>
 
         <div class="w-full bg-white shadow flex flex-col my-4 p-6">
             <p class="text-xl font-semibold pb-5">Instagram</p>
             <div class="grid grid-cols-3 gap-3">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=1">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=2">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=3">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=4">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=5">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=6">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=7">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=8">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=9">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1265838/150x150?sig=1">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1265838/150x150?sig=2">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1265838/150x150?sig=3">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1265838/150x150?sig=4">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1265838/150x150?sig=5">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1265838/150x150?sig=6">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1265838/150x150?sig=7">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1265838/150x150?sig=8">
+                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1265838/150x150?sig=9">
             </div>
             <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
-                <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb
+                <i class="fab fa-instagram mr-2"></i> Follow @cookmaster
             </a>
         </div>
 
