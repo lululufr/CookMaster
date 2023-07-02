64936da52821191a65595e28accfbed66b6cc211
lululufr <lucas.lm.miller@gmail.com>
2023-07-02 20:02:49 +0200

    Add - Creation de formation todo : creation des questionnaires

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/event', [APIController::class,'api_event_get']);
 Route::get('/recipe', [APIController::class,'api_recipe_get']);
+Route::get('/message/{id}', [APIController::class,'api_recipe_get']);
+Route::get('/conversation', [APIController::class,'api_recipe_get']);
+Route::get('/conversation', [APIController::class,'api_send_messages_get']);
