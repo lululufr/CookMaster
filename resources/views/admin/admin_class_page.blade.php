6693ed492b1d9ac4017b84c46c443dadcbbfe2fc
lululufr <lucas.lm.miller@gmail.com>
2023-07-11 17:22:43 +0200

    ADD - front formation

--- resources/views/admin/admin_class_page.blade.php
+++ resources/views/admin/admin_class_page.blade.php
@@ -4,16 +4,18 @@
     $(document).ready(function() {
         $('#genererFormulaire').click(function() {
             var formulaire = `
-            <div class="card bg-base-300">
+            <div class="card bg-base-200 m-3">
                 <div class="elementFormulaire">
+                    <b>Chapitre :</b>
+
                     <label for="titre">Titre :</label>
-                    <input type="text" name="titre[]" required><br><br>
+                    <input class="input w-full max-w-xs m-2" type="text" name="titre[]" required><br><br>
 
                     <label for="contenu">Contenu :</label>
-                    <textarea name="contenu[]" required></textarea><br><br>
+                    <textarea class="textarea textarea-bordered textarea-lg w-full max-w-xs" name="contenu[]" required></textarea><br><br>
 
                     <label for="media">Vidéo ou Image :</label>
-                    <input type="file" name="media[]"><br><br>
+                    <input class="file-input w-full max-w-xs" type="file" name="media[]"><br><br>
 
                     <button class="supprimerFormulaire" type="button">X</button>
                 </div>
@@ -28,45 +30,69 @@
         });
     });
 </script>
-<div class="form-control w-full max-w-xs m-5">
-    <form class="grid " action="/admin/create/class" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="card bg-base-300">
-            <!-- Partie Classes -->
-            <div>
-                <input type="text" name="title" placeholder="Titre de la formation" required/>
+
+
+
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Créer une formation.</h1>
+            <p class="py-6">Créez une formation, avec le plus de detail possible. Tous les revenu iront a une association.</p>
+
+            <img src="/images/deco/undraw_educator_re_ju47.svg"/>
+
+
+            <div class="form-control w-full max-w-xs m-5">
+                <form class="grid " action="/admin/create/class" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="card bg-base-200">
+                        <!-- Partie Classes -->
+                        <div>
+                            <input class="input w-full max-w-xs" type="text" name="title" placeholder="Titre de la formation" required/>
+                        </div>
+
+                        <div>
+                            <textarea class="textarea textarea-bordered textarea-lg w-full max-w-xs" name="description" placeholder="Description de la formation" required></textarea>
+                        </div>
+
+
+                        <label for="media">Image de couverture :</label>
+                        <input class="file-input w-full max-w-xs" type="file" name="media_classes"><br><br>
+                        <!-- Partie Classes -->
+                    </div>
+
+                    <div class="card bg-base-200 w-full">
+                        <b>Chapitre :</b>
+                        <div class="elementFormulaire">
+                            <label for="titre">Titre :</label>
+                            <input class="input w-full max-w-xs" type="text" name="titre[]" required><br><br>
+
+                            <label for="contenu">Contenu :</label>
+                            <textarea class="textarea textarea-bordered textarea-lg w-full max-w-xs" name="contenu[]" required></textarea><br><br>
+
+                            <label for="media">Vidéo ou Image :</label>
+                            <input class="file-input w-full max-w-xs" type="file" name="media[]"><br><br>
+
+                        </div>
+                    </div>
+
+
+                    <div id="formulairesGeneres"></div>
+
+                    <button class="btn" type="button" id="genererFormulaire">Ajouter Chapitre</button>
+
+                    <input class="btn btn-primary" type="submit" value="Creer la formation !!">
+                </form>
             </div>
 
-            <div>
-                <textarea name="description" placeholder="Description de la formation" required></textarea>
-            </div>
 
 
-            <label for="media">Image de couverture :</label>
-            <input type="file" name="media_classes"><br><br>
-        <!-- Partie Classes -->
+
+
         </div>
-
-    <div class="card bg-base-300 w-full">
-            <div class="elementFormulaire">
-                <label for="titre">Titre :</label>
-                <input type="text" name="titre[]" required><br><br>
-
-                <label for="contenu">Contenu :</label>
-                <textarea name="contenu[]" required></textarea><br><br>
-
-                <label for="media">Vidéo ou Image :</label>
-                <input type="file" name="media[]"><br><br>
-
-            </div>
     </div>
-
-
-        <div id="formulairesGeneres"></div>
-
-        <button class="btn" type="button" id="genererFormulaire">Ajouter</button>
-
-        <input class="btn" type="submit" value="Creer la formation !!">
-    </form>
 </div>
+
+
+
 <x-footer/>
