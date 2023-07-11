0ade82e4b16e5f17ed1f7811709b991364d03b3e
Alexis <alexis.elefteriou.pro@gmail.com>
2023-07-11 11:15:33 +0200

    Add - API now needs token todo :

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -17,80 +17,102 @@ use Illuminate\Support\Facades\DB;
 
 class APIController extends Controller
 {
-    public function api_event_get()
+    public function api_event_get(Request $request)
     {
-        $events = Event::with(['rooms', 'users_count'])->where('is_validated',1)->get();
+        if($this->is_connected($request->bearerToken())) {
+            $events = Event::with(['rooms', 'users_count'])->where('is_validated', 1)->get();
 
-        foreach ($events as $event) {
-            $event->rooms;
-            $event->users_count;
+            foreach ($events as $event) {
+                $event->rooms;
+                $event->users_count;
+            }
+            return response()->json([
+                'events' => $events
+            ], 200);
         }
         return response()->json([
-            'events' => $events
-        ], 200);
+            'message' => 'Bad credentials'
+        ], 401);
     }
 
-    public function api_recipe_get(){
-        $recipes = Recipes::with('tags', 'ingredients')->get();
+    public function api_recipe_get(Request $request){
 
-        return response()->json([
-            'recipes' => $recipes
-        ], 200);
-    }
-
-    public function api_message_get(int $id){
-
-        $loggedInUserId = auth()->user()->id;
-
-        $messages = Messages::where(function ($query) use ($loggedInUserId, $id) {
-            $query->where('from_id', $loggedInUserId)
-                ->where('to_id', $id);
-        })->orWhere(function ($query) use ($loggedInUserId, $id) {
-            $query->where('from_id', $id)
-                ->where('to_id', $loggedInUserId);
-        })->get();
-
-
-        return response()->json([
-            'messages' => $messages
-        ], 200);
-    }
-
-    public function api_conversation_get(){
-
-
-        $messages = Messages::where('to_id', auth()->user()->id)->orWhere('from_id', auth()->user()->id)->get();
-
-        $conversation = array();
-
-        foreach ($messages as $message) {
-            if (!in_array($message->from_id,$conversation) && $message->from_id != auth()->user()->id) {
-                array_push($conversation, $message->from_id);
-            }
-            if (!in_array($message->to_id,$conversation) && $message->to_id != auth()->user()->id) {
-                array_push($conversation, $message->to_id);
-            }
-
+        if($this->is_connected($request->bearerToken())) {
+            $recipes = Recipes::with('tags', 'ingredients')->get();
+            return response()->json([
+                'recipes' => $recipes
+            ], 200);
         }
-
-        $convs = User::whereIn('id', $conversation)->get();
-
-
         return response()->json([
-            '$convs' => $convs
-        ], 200);
+            'message' => 'Bad credentials'
+        ], 401);
+    }
+
+    public function api_message_get(Request $request, int $id){
+
+        if($this->is_connected($request->bearerToken())) {
+            $loggedInUserId = auth()->user()->id;
+            $messages = Messages::where(function ($query) use ($loggedInUserId, $id) {
+                $query->where('from_id', $loggedInUserId)
+                    ->where('to_id', $id);
+            })->orWhere(function ($query) use ($loggedInUserId, $id) {
+                $query->where('from_id', $id)
+                    ->where('to_id', $loggedInUserId);
+            })->get();
+
+
+            return response()->json([
+                'messages' => $messages
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Bad credentials'
+        ], 401);
+    }
+
+    public function api_conversation_get(Request $request){
+
+        if($this->is_connected($request->bearerToken())) {
+            $messages = Messages::where('to_id', auth()->user()->id)->orWhere('from_id', auth()->user()->id)->get();
+
+            $conversation = array();
+
+            foreach ($messages as $message) {
+                if (!in_array($message->from_id, $conversation) && $message->from_id != auth()->user()->id) {
+                    array_push($conversation, $message->from_id);
+                }
+                if (!in_array($message->to_id, $conversation) && $message->to_id != auth()->user()->id) {
+                    array_push($conversation, $message->to_id);
+                }
+
+            }
+
+            $convs = User::whereIn('id', $conversation)->get();
+
+
+            return response()->json([
+                '$convs' => $convs
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Bad credentials'
+        ], 401);
     }
 
     public function api_send_messages(Request $request, int $id)
     {
-        $message =  $request['message'];
+        if($this->is_connected($request->bearerToken())) {
+            $message = $request['message'];
 
-        $entry = new Messages();
-        $entry->to_id = $id;
-        $entry->from_id = auth()->user()->id;
-        $entry->content = $message;
-        $entry->save();
-
+            $entry = new Messages();
+            $entry->to_id = $id;
+            $entry->from_id = auth()->user()->id;
+            $entry->content = $message;
+            $entry->save();
+        }
+        return response()->json([
+            'message' => 'Bad credentials'
+        ], 401);
     }
 
     public function api_connexion(Request $request)
@@ -113,5 +135,13 @@ class APIController extends Controller
             'role' => $user->role,
             'id' => $user->id
         ], 200);
+    }
+
+    public function is_connected($token){
+        $user = User::where('mobile_token', $token)->first();
+        if($user){
+            return true;
+        }
+        return false;
     }
 }
