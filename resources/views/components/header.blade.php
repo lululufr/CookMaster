e29699ec2ce3e15e61f087ed0ff6884dea6d5329
Alexis <alexis.elefteriou.pro@gmail.com>
2023-07-12 17:17:45 +0200

    Add - cannot create recipe if user todo :

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -125,7 +125,9 @@
     <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
         <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
             <a href="/getevent" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">{{__("Planning")}}</a>
-            <a href="/recipe/create" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">{{__("Nouvelle recette")}}</a>
+            @if(auth()->user()->role != "user")
+                <a href="/recipe/create" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">{{__("Nouvelle recette")}}</a>
+            @endif
 
             <a href="/class" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">{{__("Formation")}}</a>
             <a href="/" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">{{__("Recette")}}</a>
